fe38c1a254615b7c5853b1ee16a17a9ec3e4ec9e
Jose Castillo <exmo800@gmail.com>
2019-03-29 00:37:40 -0400

    Quiz files
    Color changing triangle

---
+++ Assets/scripts/ColorChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour {
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+
+		transform.GetComponent<Renderer> ().material.color = other.GetComponent<SpriteRenderer> ().color;
+
+	}
+}
